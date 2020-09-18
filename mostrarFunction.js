8c859bfde640114c03156b26cd031e66d0e8a62f
Diego Do Santos <ddosantos@hotmail.com>
2020-09-17 22:26:16 -0300

    Muestro Productos en carrito

---
+++ mostrarFunction.js
@@ -0,0 +1,17 @@
+function mostrar(lista) {
+
+	var tabla = `<table class="table mt-5">
+	<thead>
+		<tr>
+	  	<th scope="col">Nombres</th>
+	  	<th scope="col">Precio</th>
+		</tr>
+	</thead>
+	<tbody>`;
+	for (let item of lista) {
+		tabla += `<tr><td>${item.nombre}</td><td>${item.precio}</td></tr>`;
+	}
+	tabla += `</tbody></table>`;
+
+	document.getElementById('lista').innerHTML = tabla;
+}
